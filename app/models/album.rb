81944d3f0de523f67dbfe9fbefb3755cfbf9023d
Vikas <vikasd02@gmail.com>
2016-07-19 11:34:34 -0700

    Task6 - Added validates presence on association
    Task6 - Added validates presence true on publisher association so that
    it will check if publisher is present or not before saving Album to
    database.

--- app/models/album.rb
+++ app/models/album.rb
@@ -4,7 +4,8 @@ class Album < ActiveRecord::Base
   
   validates :name, presence: true, length: { in: 2..50 }
   validates :cover_art, presence: true
-  validates :released_on, presence: true, format: { with: /\A\d{4}-\d{2}-\d{2}\z/, message: "should be in the format YYYY-MM-DD" }
+  validates :released_on, presence: true, format: { with: /\A\d{4}-\d{2}-\d{2}\z/, message: "should be in the format YYYY-MM-DD" } 
+  validates :publisher, presence: true 
   
   scope :newest, lambda { order(released_on: :desc).first }
   scope :recent, lambda {|n| where("released_on >= ?", 6.months.ago).order(released_on: :desc).first(n)}
