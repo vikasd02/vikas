876d864a3ec254b7dcd4124e1f4ac309280417b7
Vikas <vikasd02@gmail.com>
2016-07-19 11:05:05 -0700

    Task4 - Added a new scope recent that takes a argument.
    Task4 - Added a scope 'recent' to Album model and it will take an
    argument. Used lambda and also it takes an argument which is passed on
    to first method.
    (Also fixed Task1 here as I had to use DESC order and active record
    order method defaults to ASC order).

--- app/models/album.rb
+++ app/models/album.rb
@@ -6,5 +6,6 @@ class Album < ActiveRecord::Base
   validates :cover_art, presence: true
   validates :released_on, presence: true, format: { with: /\A\d{4}-\d{2}-\d{2}\z/, message: "should be in the format YYYY-MM-DD" }
   
-  scope :newest, lambda { order(:released_on).first }
+  scope :newest, lambda { order(released_on: :desc).first }
+  scope :recent, lambda {|n| where("released_on >= ?", 6.months.ago).order(released_on: :desc).first(n)}
 end
