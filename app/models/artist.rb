80381515a0e0d03f1704fcb0b0522cd4f7283710
Vikas <vikasd02@gmail.com>
2016-07-19 12:41:51 -0700

    Task9 - Added uniq lambda call to has_many through association
    Task9 -  Added uniq lambda call to has_many through association. An
    album can have many songs and an artist can have many songs. So we are
    getting albums repeated.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,6 +1,6 @@
 class Artist < ActiveRecord::Base
   has_many :songs
-  has_many :albums, through: :songs
+  has_many :albums, -> { uniq }, through: :songs
   
   validates :name, presence: true
 end
